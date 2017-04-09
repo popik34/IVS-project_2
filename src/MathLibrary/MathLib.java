68d9460ce2399d764bedb2255ba456bddedad59e
xsamek06 <xsamek06@stud.fit.vutbr.cz>
2017-04-09 21:35:52 +0200

    Obecná odmocnina, doplněny dokumentační komentáře

--- src/MathLibrary/MathLib.java
+++ src/MathLibrary/MathLib.java
@@ -6,6 +6,57 @@ package MathLibrary;
  */
 public class MathLib {
     
+	/**
+	 * Sečte dvě čísla
+	 * @param a
+	 * @param b
+	 * @return a + b
+	 */
+	public static double addition (double a, double b){
+		return a + b;
+	}
+	
+	/**
+	 * Odečte dvě čísla
+	 * @param a
+	 * @param b
+	 * @return a - b
+	 */
+	public static double subtraction (double a, double b){
+		return a - b;
+	} 
+	
+	/**
+	 * Vynásobí dvě čísla
+	 * @param a
+	 * @param b
+	 * @return a * b
+	 */
+	public static double multiplication (double a, double b){
+		return a * b;
+	}
+	
+	/**
+	 * Vydělí dvě čísla
+	 * @param a
+	 * @param b
+	 * @return a / b
+	 * @throws IllegalArgumentException při dělení nulou
+	 */
+	public static double division (double a, double b){
+		if (b == 0){
+			throw new IllegalArgumentException(); 
+		}
+		
+		return a / b;
+	}
+	
+	/**
+	 * Vypočítá faktoriál zadaného čísla
+	 * @param number
+	 * @return faktoriál
+	 * @throws IllegalArgumentException pokud je číslo menší než 0
+	 */
     public static double factorial(long number) throws IllegalArgumentException {
 		if (number < 0){
 			throw new IllegalArgumentException();
@@ -22,7 +73,14 @@ public class MathLib {
 		
         return result;
     }
-        
+    
+	/**
+	 * Vypočítá n-tou mocninu čísla number
+	 * @param number
+	 * @param n přirozený exponent
+	 * @return n-tá odmocnina čísla number
+	 * @throws IllegalArgumentException pokud n není přirozené číslo
+	 */
 	public static double pow(double number, int n) {
 		if (n < 0) {
 			throw new IllegalArgumentException();
@@ -38,11 +96,29 @@ public class MathLib {
 		return result;
 	}
 	
+	/**
+	 * Provede n-tou odmocninu
+	 * @param number
+	 * @param n
+	 * @return n-tá odmocnina čísla number
+	 * @throws IlleagalArgumentException pokud je odmocněnec záporný
+	 */
     public static double root(double number, double n){
-        
-        return number;
+        if (number < 0){
+			throw new IllegalArgumentException();
+		}
+		else if (number == 0){
+			return 0;
+		}
+		
+		return Math.pow(number, 1/n);
     }
     
+	/**
+	 * Absolutní hodnota
+	 * @param number
+	 * @return absolutní hodnota čísla number
+	 */
     public static int abs(int number){
         if (number > 0){
 			return number;
@@ -51,7 +127,12 @@ public class MathLib {
 			return number * -1;
 		}
     }
-    
+ 	
+	/**
+	 * Absolutní hodnota
+	 * @param number
+	 * @return absolutní hodnota čísla number
+	 */   
     public static long abs(long number){
         if (number > 0){
 			return number;
@@ -61,18 +142,30 @@ public class MathLib {
 		}
     }
     
+	/**
+	 * Absolutní hodnota
+	 * @param number
+	 * @return absolutní hodnota čísla number
+	 */
     public static float abs(float number){
 		if (number > 0) {
 			return number;
-		} else {
+		} 
+		else {
 			return number * -1;
 		}        
     }
     
+	/**
+	 * Absolutní hodnota
+	 * @param number
+	 * @return absolutní hodnota čísla number
+	 */
     public static double abs(double number){
 		if (number > 0) {
 			return number;
-		} else {
+		} 
+		else {
 			return number * -1;
 		}        
     }
